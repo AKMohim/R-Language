b1fe9703720140b93226e6e5cb595dc01b50d472
AKMohim <akmohim9592@gmail.com>
2025-05-10 23:47:22 +0600

    Add files via upload

---
+++ 22-47833-2 Lab01 (1).R
@@ -0,0 +1,44 @@
+1+1
+3-2
+4*6
+99/11
+4**3
+9%/%6
+10%%7
+
+sqrt(64)
+4+5-8/4+1*2
+
+a<- 7
+b<- 5
+c<- 3
+d<- -1
+
+a+b-c
+e<- a+b
+print(e)
+abs(d)
+ceiling(a/c)
+floor(a/c)
+
+N<- "AiuB"
+tolower(N)
+toupper(N)
+strsplit(N,split='')
+
+l<- c(4,5,55,44,8,5,1,77,100,16,89)
+min(l)
+max(l)
+sum(l)
+mean(l)
+mode(l)
+range(l)
+sd(l)
+
+nchar(N)
+n<- "student"
+paste(n, N)
+
+X<- "this is the the first lab task
+ in the data science class"
+cat(X)
